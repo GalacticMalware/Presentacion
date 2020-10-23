8ed04f1e66be5c9dc23deaf5fd54c411a3b8e8a3
Daniel <daniel_mg_97@hotmail.com>
2020-10-22 20:17:54 -0500

    Modificacion de imagen, estilos

--- public/js/app.js
+++ public/js/app.js
@@ -59140,8 +59140,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /opt/lampp/htdocs/Presentacion/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /opt/lampp/htdocs/Presentacion/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\laragon\www\Presentacion\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\laragon\www\Presentacion\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
