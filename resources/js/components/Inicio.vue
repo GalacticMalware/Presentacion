37585dd7e6dfa4bd57ee39235b86f78bef235304
Daniel <daniel_mg_97@hotmail.com>
2022-05-24 17:05:05 -0500

    Anuncio

--- resources/js/components/Inicio.vue
+++ resources/js/components/Inicio.vue
@@ -30,9 +30,41 @@
         <img class="img-fluid mb-4" src="/img/home.svg" alt="Desarrollo" />
       </div>
     </div>
+
+
+
+<!-- Modal -->
+<div class="modal fade" id="nota" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLongTitle">Actualización de la pagina</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        Disculpe las molestias, lo que verá a continuación será información obsoleta de esta página.
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+        
+      </div>
+    </div>
+  </div>
+</div>
+
+
+    
   </div>
 </template>
 
 <script>
-export default {};
+
+export default {
+mounted(){
+  $('#nota').modal('show')
+}
+};
+
 </script>
